633eefe119317de8700819ab09c3a0563a7afda1
mlarouche <michael.larouche@gmail.com>
2023-05-24 08:13:37 -0400

    Port properly the struct and callbacks of MemoryMgr

--- src/MemoryMgr.zig
+++ src/MemoryMgr.zig
@@ -2,13 +2,133 @@ const std = @import("std");
 const c = @import("c.zig");
 const common = @import("common.zig");
 
-pub const AkMemSettings = c.WWISEC_AkMemSettings;
 pub const AkMemPoolId = c.WWISEC_AkMemPoolId;
-pub const CategoryStats = c.WWISEC_AK_MemoryMgr_CategoryStats;
-pub const GlobalStats = c.WWISEC_AK_MemoryMgr_GlobalStats;
+
+pub const AkMemID = enum(common.DefaultEnumType) {
+    object = c.WWISEC_AkMemID_Object,
+    event = c.WWISEC_AkMemID_Event,
+    structure = c.WWISEC_AkMemID_Structure,
+    media = c.WWISEC_AkMemID_Media,
+    game_object = c.WWISEC_AkMemID_GameObject,
+    processing = c.WWISEC_AkMemID_Processing,
+    processing_plugin = c.WWISEC_AkMemID_ProcessingPlugin,
+    streaming = c.WWISEC_AkMemID_Streaming,
+    streaming_io = c.WWISEC_AkMemID_StreamingIO,
+    spatial_audio = c.WWISEC_AkMemID_SpatialAudio,
+    spatial_audio_geometry = c.WWISEC_AkMemID_SpatialAudioGeometry,
+    spatial_audio_paths = c.WWISEC_AkMemID_SpatialAudioPaths,
+    game_sim = c.WWISEC_AkMemID_GameSim,
+    monitor_queue = c.WWISEC_AkMemID_MonitorQueue,
+    profiler = c.WWISEC_AkMemID_Profiler,
+    file_package = c.WWISEC_AkMemID_FilePackage,
+    sound_engine = c.WWISEC_AkMemID_SoundEngine,
+    integration = c.WWISEC_AkMemID_Integration,
+    job_mgr = c.WWISEC_AkMemID_JobMgr,
+};
+
+pub const AkMemType_Media = c.WWISEC_AkMemType_Media;
+pub const AkMemType_Device = c.WWISEC_AkMemType_Device;
+pub const AkMemType_NoTrack = c.WWISEC_AkMemType_NoTrack;
+
+pub const AkMemInitForThread = ?*const fn () callconv(.C) void;
+pub const AkMemTermForThread = ?*const fn () callconv(.C) void;
+pub const AkMemTrimForThread = ?*const fn () callconv(.C) void;
+pub const AkMemMalloc = ?*const fn (pool_id: AkMemPoolId, size: usize) callconv(.C) ?*anyopaque;
+pub const AkMemMalign = ?*const fn (pool_id: AkMemPoolId, size: usize, alignment: u32) callconv(.C) ?*anyopaque;
+pub const AkMemRealloc = ?*const fn (pool_id: AkMemPoolId, address: ?*anyopaque, size: usize) callconv(.C) ?*anyopaque;
+pub const AkMemReallocAligned = ?*const fn (pool_id: AkMemPoolId, address: ?*anyopaque, size: usize, alignment: u32) callconv(.C) ?*anyopaque;
+pub const AkMemFree = ?*const fn (pool_id: AkMemPoolId, address: ?*anyopaque) callconv(.C) void;
+pub const AkMemTotalReservedMemorySize = ?*const fn () callconv(.C) usize;
+pub const AkMemSizeOfMemory = ?*const fn (pool_id: AkMemPoolId, address: ?*anyopaque) callconv(.C) usize;
+pub const AkMemDebugMalloc = ?*const fn (pool_id: AkMemPoolId, size: usize, address: ?*anyopaque, file: ?[*:0]const u8, line: u32) callconv(.C) void;
+pub const AkMemDebugMalign = ?*const fn (pool_id: AkMemPoolId, size: usize, alignment: u32, address: ?*anyopaque, file: ?[*:0]const u8, line: u32) callconv(.C) void;
+pub const AkMemDebugRealloc = ?*const fn (pool_id: AkMemPoolId, old_address: ?*anyopaque, size: usize, new_addresss: ?*anyopaque, file: ?[*:0]const u8, line: u32) callconv(.C) void;
+pub const AkMemDebugReallocAligned = ?*const fn (pool_id: AkMemPoolId, old_address: ?*anyopaque, size: usize, alignment: u32, new_address: ?*anyopaque, file: ?[*:0]const u8, line: u32) callconv(.C) void;
+pub const AkMemDebugFree = ?*const fn (pool_id: AkMemPoolId, address: ?*anyopaque) callconv(.C) void;
+pub const AkMemAllocVM = ?*const fn (size: usize, extra: ?*usize) callconv(.C) ?*anyopaque;
+pub const AkMemFreeVM = ?*const fn (address: ?*anyopaque, size: usize, extra: usize, release: usize) callconv(.C) void;
+
+pub const AkMemSettings = extern struct {
+    init_for_thread: AkMemInitForThread = null,
+    term_for_thread: AkMemTermForThread = null,
+    malloc: AkMemMalloc = null,
+    malign: AkMemMalign = null,
+    realloc: AkMemRealloc = null,
+    realloc_aligned: AkMemReallocAligned = null,
+    free: AkMemFree = null,
+    total_reserved_memory_size: AkMemTotalReservedMemorySize = null,
+    size_of_memory: AkMemSizeOfMemory = null,
+    mem_allocation_size_limit: u64 = 0,
+    use_device_mem_always: bool = false,
+    alloc_vm: AkMemAllocVM = null,
+    free_vm: AkMemFreeVM = null,
+    alloc_device: AkMemAllocVM = null,
+    free_device: AkMemFreeVM = null,
+    vm_page_size: u32 = 0,
+    device_page_size: u32 = 0,
+    debug_malloc: AkMemDebugMalloc = null,
+    debug_malign: AkMemDebugMalign = null,
+    debug_realloc: AkMemDebugRealloc = null,
+    debug_realloc_aligned: AkMemDebugReallocAligned = null,
+    debug_free: AkMemDebugFree = null,
+    memory_debug_level: u32 = 0,
+    trim_for_thread: AkMemTrimForThread = null,
+
+    pub inline fn fromC(value: c.WWISEC_AkMemSettings) AkMemSettings {
+        return @bitCast(AkMemSettings, value);
+    }
+
+    pub inline fn toC(self: AkMemSettings) c.WWISEC_AkMemSettings {
+        return @bitCast(c.WWISEC_AkMemSettings, self);
+    }
+
+    comptime {
+        std.debug.assert(@sizeOf(AkMemSettings) == @sizeOf(c.WWISEC_AkMemSettings));
+    }
+};
+
+pub const CategoryStats = extern struct {
+    used: u64 = 0,
+    peak_used: u64 = 0,
+    allocs: u32 = 0,
+    frees: u32 = 0,
+
+    pub inline fn fromC(value: c.WWISEC_AK_MemoryMgr_CategoryStats) CategoryStats {
+        return @bitCast(CategoryStats, value);
+    }
+
+    pub inline fn toC(self: CategoryStats) c.WWISEC_AK_MemoryMgr_CategoryStats {
+        return @bitCast(c.WWISEC_AK_MemoryMgr_CategoryStats, self);
+    }
+
+    comptime {
+        std.debug.assert(@sizeOf(CategoryStats) == @sizeOf(c.WWISEC_AK_MemoryMgr_CategoryStats));
+    }
+};
+
+pub const GlobalStats = extern struct {
+    used: u64 = 0,
+    device_used: u64 = 0,
+    reserved: u64 = 0,
+    max: u64 = 0,
+
+    pub inline fn fromC(value: c.WWISEC_AK_MemoryMgr_GlobalStats) GlobalStats {
+        return @bitCast(GlobalStats, value);
+    }
+
+    pub inline fn toC(self: GlobalStats) c.WWISEC_AK_MemoryMgr_GlobalStats {
+        return @bitCast(c.WWISEC_AK_MemoryMgr_GlobalStats, self);
+    }
+
+    comptime {
+        std.debug.assert(@sizeOf(GlobalStats) == @sizeOf(c.WWISEC_AK_MemoryMgr_GlobalStats));
+    }
+};
 
 pub fn init(in_pSettings: *AkMemSettings) common.WwiseError!void {
-    return common.handleAkResult(c.WWISEC_AK_MemoryMgr_Init(@ptrCast([*c]AkMemSettings, in_pSettings)));
+    return common.handleAkResult(
+        c.WWISEC_AK_MemoryMgr_Init(@ptrCast(*c.WWISEC_AkMemSettings, in_pSettings)),
+    );
 }
 
 pub fn term() void {
@@ -16,7 +136,7 @@ pub fn term() void {
 }
 
 pub fn getDefaultSettings(out_pMemSettings: *AkMemSettings) void {
-    c.WWISEC_AK_MemoryMgr_GetDefaultSettings(out_pMemSettings);
+    c.WWISEC_AK_MemoryMgr_GetDefaultSettings(@ptrCast(*c.WWISEC_AkMemSettings, out_pMemSettings));
 }
 
 pub fn isInitialized() bool {
@@ -48,15 +168,15 @@ pub fn malign(in_poolId: AkMemPoolId, in_USize: usize, in_uAlignment: u32) *anyo
 }
 
 pub fn getCategoryStats(in_poolId: AkMemPoolId) CategoryStats {
-    var result: CategoryStats = undefined;
-    c.WWISEC_AK_MemoryMgr_GetCategoryStats(in_poolId, &result);
-    return result;
+    var raw_result: CategoryStats = undefined;
+    c.WWISEC_AK_MemoryMgr_GetCategoryStats(in_poolId, &raw_result);
+    return CategoryStats.fromC(raw_result);
 }
 
 pub fn getGlobalStats() GlobalStats {
-    var result: GlobalStats = undefined;
-    c.WWISEC_AK_MemoryMgr_GetGlobalStats(&result);
-    return result;
+    var raw_result: GlobalStats = undefined;
+    c.WWISEC_AK_MemoryMgr_GetGlobalStats(&raw_result);
+    return GlobalStats.fromC(raw_result);
 }
 
 pub fn startProfileThreadUsage() void {
